9dc7b0fd17c6068e4aa3f8e4d00b3beabf44ce5e
ish-suarez <87841664+ish-suarez@users.noreply.github.com>
2023-04-10 11:18:39 -0500

    updated text size for footer signature

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -1,15 +1,15 @@
 export default function Footer () {
     return(
         <footer className="flex flex-col justify-center py-10 text-center">
-            <a className="self-center w-1/5 transition delay-100 hover:scale-110" target={'_blank'} rel={'noreferrer'} href="https://www.facebook.com/TaqueriaMedrano">
+            <a className="self-center w-1/4 transition delay-100 hover:scale-110" target={'_blank'} rel={'noreferrer'} href="https://www.facebook.com/TaqueriaMedrano">
                 <img className="" width={25} height={25} src="https://img.icons8.com/color/48/null/facebook-new.png" alt="Facebook logo"/>
 
             </a>
-            <div>
-                <p className="text-sm text-gray-200">
+            <div className="font-light text-[8px] py-5">
+                <p className=" text-gray-200">
                     Designed by <a className="text-white hover:text-gray-300" href="https://izzydev.com" target={'_blank'} rel={'noreferrer'}>izzydev.com</a>
                 </p>
-                <p className="text-xs text-gray-300 ">Icons source <a className="text-white hover:text-gray-300" href="https://www.icons8.com" target={'_blank'} rel='noreferrer'>icons8.com</a></p>
+                <p className=" text-gray-300 ">Icons source <a className="text-white hover:text-gray-300" href="https://www.icons8.com" target={'_blank'} rel='noreferrer'>icons8.com</a></p>
 
             </div>
         </footer>
