33d47cb71533e2e66ea62d9f28f495e314059fee
ish-suarez <87841664+ish-suarez@users.noreply.github.com>
2023-04-14 11:24:49 -0500

    favicon issue

--- client/components/layout/layout.tsx
+++ client/components/layout/layout.tsx
@@ -12,10 +12,10 @@ export default function Layout({children, title='Medranos Taqueria'}: Props) {
         <div className="w-full subpixel-antialiased">
             <Head>
                 <title>{title}</title>
-                <link rel="shortcut icon" href="/favicon.ico" />
-                {/* <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/> */}
+                <link rel="shortcut icon" href="/images/favicons/favicon.ico" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/images/favicons/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/images/favicons/favicon-32x32.png"/>
+                <link rel="icon" type="image/png" sizes="16x16" href="/images/favicons/favicon-16x16.png"/>
             </Head>
             <Navbar />
             <main className="w-full">
