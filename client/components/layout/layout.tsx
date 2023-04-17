aa366990580c751c5bd50b28223e57122b10aac8
ish-suarez <87841664+ish-suarez@users.noreply.github.com>
2023-04-17 16:51:08 -0500

    favicon

--- client/components/layout/layout.tsx
+++ client/components/layout/layout.tsx
@@ -13,15 +13,8 @@ export default function Layout({children, title='Medranos Taqueria'}: Props) {
             <Head>
                 <title>{title}</title>
                 <meta property="og:title" content="Medrano's Taqueria" key="title" />
-                <link rel="icon" href="/images/favicons/favicon.ico" />
-                <link rel="apple-touch-icon" sizes="180x180" href="/images/favicons/apple-touch-icon.png" />
-                <link rel="icon" type="image/png" sizes="32x32" href="/images/favicons/favicon-32x32.png" />
-                <link rel="icon" type="image/png" sizes="16x16" href="/images/favicons/favicon-16x16.png" />
-                <link rel="manifest" href="/images/favicons/site.webmanifest" />
-                <link rel="mask-icon" href="/images/favicons/safari-pinned-tab.svg" color="#5bbad5" />
-                <meta name="msapplication-TileColor" content="#da532c"/>
-                <meta name="keywords" content="Tacos, Quesadillas, Burritos, Street Tacos, Taco Truck, Culture, Caldos, Sopes, Medrano's, Taqueira" />
                 <meta property="og:description" content=">Medrano's Taquria brings you fresh and authentic flavors from our culture, to you. " />
+                <meta name="keywords" content="Tacos, Quesadillas, Burritos, Street Tacos, Taco Truck, Culture, Caldos, Sopes, Medrano's, Taqueira" />
                 <meta name="theme-color" content="#2D3142" />
             </Head>
             <Navbar />
