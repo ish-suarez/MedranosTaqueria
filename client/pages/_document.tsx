272259bf1d0d013ce1dcab63140c6bfbd98dc2b4
ish-suarez <87841664+ish-suarez@users.noreply.github.com>
2023-04-17 17:39:11 -0500

    favicon

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -4,13 +4,13 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" type="image/x-icon" href="/images/favicons/favicon.ico" />
+        {/* <link rel="icon" type="image/x-icon" href="/images/favicons/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/images/favicons/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/images/favicons/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/images/favicons/favicon-16x16.png" />
         <link rel="manifest" href="/images/favicons/site.webmanifest" />
         <link rel="mask-icon" href="/images/favicons/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#da532c"/>
+        <meta name="msapplication-TileColor" content="#da532c"/> */}
       </Head>
       <body>
         <Main />
