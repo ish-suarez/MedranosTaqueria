41495dcbb28f384f8eca4106a168e0376d7c122c
ish-suarez <87841664+ish-suarez@users.noreply.github.com>
2023-04-17 16:36:18 -0500

    favicon

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -10,18 +10,6 @@ const backgroundPic = '/images/taco_truck.avif';
 export default function Home() {
   return (
     <div className='w-full'>
-      <Head>
-        <meta property="og:title" content="Medrano's Taqueria" key="title" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/images/favicons/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicons/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicons/favicon-16x16.png" />
-        <link rel="manifest" href="/images/favicons/site.webmanifest" />
-        <link rel="mask-icon" href="/images/favicons/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#da532c"/>
-        <meta name="keywords" content="Tacos, Quesadillas, Burritos, Street Tacos, Taco Truck, Culture, Caldos, Sopes, Medrano's, Taqueira" />
-        <meta property="og:description" content=">Medrano's Taquria brings you fresh and authentic flavors from our culture, to you. " />
-        <meta name="theme-color" content="#2D3142" />
-      </Head>
       {/* Background Image Container */}
       <div className={` fixed -z-10 w-full h-screen`}>
         <Image
