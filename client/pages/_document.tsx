9bd7cf842ae4b5f6eaa45f1252911f4f9f83a932
ish-suarez <87841664+ish-suarez@users.noreply.github.com>
2023-04-18 20:53:53 -0500

    changes in head tags

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -1,11 +1,11 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document"
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <meta name="theme-color" content="#2D3142" />
-        <link rel='shortcut icon' type='image/x-icon' href='/images/favicons/favicon.ico' />
+        <link rel="icon" type="image/x-icon" href="/images/favicon.ico" />
         <link rel="canonical" href="https://medranostaqueria.com/" />
         {/* <link rel="icon" type="image/x-icon" href="/images/favicons/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/images/favicons/apple-touch-icon.png" />
