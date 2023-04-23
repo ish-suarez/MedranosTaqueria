caadd25594fb4d2638c89e24c62253bed0845ac7
ish-suarez <87841664+ish-suarez@users.noreply.github.com>
2023-04-23 13:51:47 -0500

    favicon changes

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -8,7 +8,6 @@ export default function Document() {
         <link rel="icon" type="image/x-icon" href="/images/favicon.ico" />
         <link rel="icon" href="/images/favicon.ico" />
         <link rel="canonical" href="https://medranostaqueria.com/" />
-        <link rel="icon" type="image/x-icon" href="/images/favicons/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/images/favicons/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/images/favicons/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/images/favicons/favicon-16x16.png" />
