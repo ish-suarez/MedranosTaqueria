b39742b696fd7ba4c582b95244dfa7ea4e3274e4
ish-suarez <87841664+ish-suarez@users.noreply.github.com>
2023-05-02 11:14:35 -0500

    adding keyword

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         <title>Medrano's Taqueria</title>
         <meta property="og:title" content="Medrano's Taqueria" key='title' />
         <meta name="description" content="Medrano's Taqueria brings you fresh and authentic flavors from our culture, to you, in Katy, Texas. Your favorite street taco spot. Monday - Saturday: 6AM - 10PM" />
-        <meta name="keywords" content="Tacos, Quesadillas, Burritos, Street Tacos, Taco Truck, Culture, Caldos, Sopes, Medrano's, Taqueira" />
+        <meta name="keywords" content="Tacos, Quesadillas, Burritos, Street Tacos, Taco Truck, Culture, Caldos, Sopes, Medrano's, Taqueira, Katy" />
       </Head>
       {/* Home page Content */}
       <div className={`flex flex-col items-center ${styles.content}`}>
